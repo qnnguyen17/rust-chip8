aedb2c27de44b87b1b289e7076f18a91f1d85d24
Quan Nguyen <quan.ngoc.nguyen@gmail.com>
2018-11-20 21:24:01 -0800

    Implement up to the first sprite-drawing opcode.
    Next step will be adding window + graphics to see if we can draw stuff on the screen.

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,45 @@ use self::OpCode::*;
 
 #[derive(Clone, Copy, Debug)]
 enum OpCode {
+    ADD_I_REG {
+        reg: u8,
+    },
+    CALL {
+        addr: u16,
+    },
     CLS,
-    LD_I_ADDR { val: u16 },
-    LD_MEMI_REGS { last_reg: u8 },
-    LD_VX_BYTE { reg: u8, val: u8 },
-    SNE { reg: u8, val: u8 },
+    DRW {
+        reg_x: u8,
+        reg_y: u8,
+        sprite_bytes: u8,
+    },
+    LD_I_ADDR {
+        addr: u16,
+    },
+    LD_I_DIGIT_REG {
+        reg: u8,
+    },
+    LD_MEMI_REGS {
+        last_reg: u8,
+    },
+    LD_REG_BYTE {
+        reg: u8,
+        val: u8,
+    },
+    LD_REGS_MEMI {
+        last_reg: u8,
+    },
+    JP {
+        addr: u16,
+    },
+    SE_REG_BYTE {
+        reg: u8,
+        val: u8,
+    },
+    SNE_REG_BYTE {
+        reg: u8,
+        val: u8,
+    },
     SYS,
 }
 
@@ -31,8 +65,8 @@ pub struct Chip8 {
     // State of the 16 input keys
     key: [bool; 16],
 
-    // 64*32 graphics buffer
-    graphics: [bool; 64 * 32],
+    // 64 (i.e. 8 bytes) by 32 bit graphics buffer
+    graphics: [u8; 8 * 32],
 }
 
 impl Chip8 {
@@ -49,7 +83,7 @@ impl Chip8 {
             sp: 0,
             memory: [0; 4096],
             key: [false; 16],
-            graphics: [false; 64 * 32],
+            graphics: [0; 8 * 32],
         }
     }
 
@@ -72,21 +106,44 @@ impl Chip8 {
             [0x00, 0xe0] => CLS,
             // Must come after all the other 0NNN instructions
             [0x00...0x0F, _] => SYS,
-            [msb @ 0x40...0x4F, lsb] => SNE {
-                reg: *msb & 0x0f,
+            [msb @ 0x10...0x1F, lsb] => JP {
+                addr: extract_addr(msb, lsb),
+            },
+            [msb @ 0x20...0x2F, lsb] => CALL {
+                addr: extract_addr(msb, lsb),
+            },
+            [msb @ 0x30...0x3F, lsb] => SE_REG_BYTE {
+                reg: msb & 0xF,
                 val: *lsb,
             },
-            [msb @ 0x60...0x6F, lsb] => LD_VX_BYTE {
-                reg: *msb & 0x0f,
+            [msb @ 0x40...0x4F, lsb] => SNE_REG_BYTE {
+                reg: msb & 0xF,
+                val: *lsb,
+            },
+            [msb @ 0x60...0x6F, lsb] => LD_REG_BYTE {
+                reg: msb & 0xF,
                 val: *lsb,
             },
             [msb @ 0xA0...0xAF, lsb] => LD_I_ADDR {
-                val: *lsb as u16 | ((*msb as u16 & 0x0F) << 8),
+                addr: extract_addr(msb, lsb),
             },
+            [msb @ 0xD0...0xDF, lsb] => DRW {
+                reg_x: msb & 0xF,
+                reg_y: (lsb & 0xF0) >> 4,
+                sprite_bytes: lsb & 0xF,
+            },
+            [msb @ 0xF0...0xFF, 0x1E] => ADD_I_REG { reg: msb & 0xF },
+            [msb @ 0xF0...0xFF, 0x29] => LD_I_DIGIT_REG { reg: msb & 0xF },
             [msb @ 0xF0...0xFF, 0x55] => LD_MEMI_REGS {
-                last_reg: *msb & 0x0F,
+                last_reg: msb & 0x0F,
             },
-            [msb, lsb] => panic!("Unknown op code {}", *lsb as usize | ((*msb as usize) << 8)),
+            [msb @ 0xF0...0xFF, 0x65] => LD_REGS_MEMI {
+                last_reg: msb & 0x0F,
+            },
+            [msb, lsb] => panic!(
+                "Unknown op code {:x}",
+                *lsb as usize | ((*msb as usize) << 8)
+            ),
             _ => panic!("Invalid program counter"),
         }
     }
@@ -94,40 +151,105 @@ impl Chip8 {
     fn execute(&mut self, op: OpCode) {
         let mut new_pc = self.pc + 2;
         match op {
+            ADD_I_REG { reg } => {
+                let reg_val = self.get_reg_val(reg);
+                println!("Adding {:x} to I's current value {:x}", reg_val, self.i);
+                self.i += reg_val as u16;
+            }
+            CALL { addr } => {
+                println!(
+                    "Storing current PC {:x} on the stack and jumping to {:x}",
+                    self.pc, addr
+                );
+                self.stack[self.sp as usize] = self.pc;
+                self.sp += 1;
+                new_pc = addr;
+            }
             CLS => {
                 println!("Clearing screen");
-                self.graphics = [false; 64 * 32];
+                self.graphics = [0; 8 * 32];
             }
-            LD_I_ADDR { val } => {
-                let address = val & 0xFFF;
-                println!("Loading reg I with address {:x}", address);
-                self.i = address;
+            DRW {
+                reg_x,
+                reg_y,
+                sprite_bytes,
+            } => {
+                let i = self.i;
+                let x = self.get_reg_val(reg_x);
+                let y = self.get_reg_val(reg_y);
+                println!(
+                    "Drawing {} bytes of sprite from address {:x} at location {},{} on the screen",
+                    sprite_bytes, i, x, y
+                );
+                // TODO: implement graphical changes
+            }
+            LD_I_ADDR { addr } => {
+                println!("Loading reg I with address {:x}", addr);
+                self.i = addr;
+            }
+            LD_I_DIGIT_REG { reg } => {
+                let sprite_digit = self.get_reg_val(reg);
+                let addr = 5 * sprite_digit as u16;
+                println!(
+                    "Loading I with address {:x}, where sprite digit {:x} is stored",
+                    addr, sprite_digit
+                );
+                self.i = addr;
             }
             LD_MEMI_REGS { last_reg } => {
                 println!(
                     "Copying regs 0 through {} into memory address {:x}",
                     last_reg, self.pc
                 );
-                for i in 0..=(last_reg as usize) {
-                    self.memory[self.pc as usize] = self.v[i];
+                for i in 0..=last_reg {
+                    self.memory[(self.i + i as u16) as usize] = self.get_reg_val(i);
                 }
             }
-            LD_VX_BYTE { reg, val } => {
+            LD_REG_BYTE { reg, val } => {
                 println!("Loading reg V{} with value {:x}", reg, val);
                 self.v[reg as usize] = val;
             }
-            SNE { reg, val } => {
-                if self.v[reg as usize] != val {
-                    println!(
-                        "Skipping next instr because {} != {}",
-                        self.v[reg as usize], val
-                    );
+            LD_REGS_MEMI { last_reg } => {
+                println!(
+                    "Loading regs 0 through {} with data in memory starting at address {:x}",
+                    last_reg, self.i
+                );
+                for i in 0..=last_reg as usize {
+                    self.v[i] = self.memory[self.i as usize + i]
+                }
+            }
+            JP { addr } => {
+                println!("Jumping to address {:x} instead of {:x}", addr, new_pc);
+                new_pc = addr;
+            }
+            SE_REG_BYTE { reg, val } => {
+                let reg_val = self.get_reg_val(reg);
+                if reg_val == val {
+                    println!("Skipping next instr because {} == {}", reg_val, val);
                     new_pc += 2;
                 }
             }
-            SYS => println!("SYS instruction found, ignoring"),
+            SNE_REG_BYTE { reg, val } => {
+                let reg_val = self.get_reg_val(reg);
+                if reg_val != val {
+                    println!("Skipping next instr because {} != {}", reg_val, val);
+                    new_pc += 2;
+                }
+            }
+            SYS => {}
+            // SYS => println!("SYS instruction found, ignoring"),
         }
 
         self.pc = new_pc;
     }
+
+    fn get_reg_val(&mut self, reg: u8) -> u8 {
+        return self.v[reg as usize];
+    }
+}
+
+// Extracts the least significant 12 bits out of the two bytes representing a
+// memory address (possibly part of an instruction).
+fn extract_addr(msb: &u8, lsb: &u8) -> u16 {
+    *lsb as u16 | ((*msb as u16 & 0x0F) << 8)
 }
