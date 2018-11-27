b57c19a4b4db6637786dcbc0d97e62ce8373980b
Quan Nguyen <quan.ngoc.nguyen@gmail.com>
2018-11-27 00:33:38 -0800

    Name the window and processor threads

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,22 @@ fn main() {
     let (graphics_bus_out, graphics_bus_in) = channel();
 
     // TODO: gracefully handle failure/abort
-    let window_thread = thread::spawn(move || {
-        let mut window = window::WindowHandler::new(graphics_bus_in);
-        window.run();
-    });
+    let window_thread = thread::Builder::new()
+        .name("window".to_string())
+        .spawn(move || {
+            let mut window = window::WindowHandler::new(graphics_bus_in);
+            window.run();
+        })
+        .expect("failed to spawn window thread");
 
-    let processor_thread = thread::spawn(move || {
-        let mut processor = cpu::CPU::new(graphics_bus_out);
-        processor.load_game_data("15PUZZLE").unwrap();
-        processor.run();
-    });
+    let processor_thread = thread::Builder::new()
+        .name("processor".to_string())
+        .spawn(move || {
+            let mut processor = cpu::CPU::new(graphics_bus_out);
+            processor.load_game_data("15PUZZLE").unwrap();
+            processor.run();
+        })
+        .expect("failed to spawn processor thread");
 
     window_thread.join().unwrap();
     processor_thread.join().unwrap();
