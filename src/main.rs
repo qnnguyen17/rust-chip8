ed86b15476047d2f143449cf913047bd3f370f2a
Quan Nguyen <quan.ngoc.nguyen@gmail.com>
2018-11-27 00:47:20 -0800

    Get filename from command line arguments

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,19 @@ mod cpu;
 mod digits;
 mod window;
 
+use std::env;
 use std::sync::mpsc::channel;
 use std::thread;
 
 fn main() {
     let (graphics_bus_out, graphics_bus_in) = channel();
+
+    let args: Vec<String> = env::args().collect();
+    let filename = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        String::from("BRIX")
+    };
 
     // TODO: gracefully handle failure/abort
     let window_thread = thread::Builder::new()
@@ -21,7 +29,7 @@ fn main() {
         .name("processor".to_string())
         .spawn(move || {
             let mut processor = cpu::CPU::new(graphics_bus_out);
-            processor.load_game_data("15PUZZLE").unwrap();
+            processor.load_game_data(&filename).unwrap();
             processor.run();
         })
         .expect("failed to spawn processor thread");
